09c780bc3e4374e896681d153f781b8beed2f5f7
Alex Edwards <ajmedwards@gmail.com>
2016-08-19 18:17:55 +0200

    [tests] Make tests consistent with other engines

--- engine/memstore/memstore_test.go
+++ engine/memstore/memstore_test.go
@@ -2,6 +2,7 @@ package memstore
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 	"time"
 
@@ -22,9 +23,9 @@ func TestNew(t *testing.T) {
 
 func TestFind(t *testing.T) {
 	m := New(time.Minute)
-	m.Cache.Set("test_session_token", []byte("encoded_data"), 0)
+	m.Cache.Set("session_token", []byte("encoded_data"), 0)
 
-	b, found, err := m.Find("test_session_token")
+	b, found, err := m.Find("session_token")
 	if err != nil {
 		t.Fatalf("got %v: expected %v", err, nil)
 	}
@@ -34,68 +35,106 @@ func TestFind(t *testing.T) {
 	if bytes.Equal(b, []byte("encoded_data")) == false {
 		t.Fatalf("got %v: expected %v", b, []byte("encoded_data"))
 	}
+}
 
-	b, found, err = m.Find("missing_session_token")
+func TestFindMissing(t *testing.T) {
+	m := New(time.Minute)
+
+	_, found, err := m.Find("missing_session_token")
 	if err != nil {
 		t.Fatalf("got %v: expected %v", err, nil)
 	}
 	if found != false {
 		t.Fatalf("got %v: expected %v", found, false)
 	}
-	if b != nil {
-		t.Fatalf("got %v: expected %v", b, nil)
-	}
 }
 
 func TestFindBadData(t *testing.T) {
 	m := New(time.Minute)
-	m.Cache.Set("test_session_token", "not_a_byte_slice", 0)
+	m.Cache.Set("session_token", "not_a_byte_slice", 0)
 
-	_, _, err := m.Find("test_session_token")
+	_, _, err := m.Find("session_token")
 	if err != errTypeAssertionFailed {
 		t.Fatalf("got %v: expected %v", err, errTypeAssertionFailed)
+	}
+}
+
+func TestSaveNew(t *testing.T) {
+	m := New(time.Minute)
+
+	err := m.Save("session_token", []byte("encoded_data"), time.Now().Add(time.Minute))
+	if err != nil {
+		t.Fatalf("got %v: expected %v", err, nil)
+	}
+
+	v, found := m.Cache.Get("session_token")
+	if found != true {
+		t.Fatalf("got %v: expected %v", found, true)
+	}
+	b, ok := v.([]byte)
+	if ok == false {
+		t.Fatal("could not convert to []byte")
+	}
+	if reflect.DeepEqual(b, []byte("encoded_data")) == false {
+		t.Fatalf("got %v: expected %v", b, []byte("encoded_data"))
+	}
+}
+
+func TestSaveUpdated(t *testing.T) {
+	m := New(time.Minute)
+	m.Cache.Set("session_token", []byte("encoded_data"), 0)
+
+	err := m.Save("session_token", []byte("encoded_data"), time.Now().Add(time.Minute))
+	if err != nil {
+		t.Fatalf("got %v: expected %v", err, nil)
+	}
+
+	err = m.Save("session_token", []byte("new_encoded_data"), time.Now().Add(time.Minute))
+	if err != nil {
+		t.Fatalf("got %v: expected %v", err, nil)
+	}
+
+	v, _ := m.Cache.Get("session_token")
+	b, ok := v.([]byte)
+	if ok == false {
+		t.Fatal("could not convert to []byte")
+	}
+	if reflect.DeepEqual(b, []byte("new_encoded_data")) == false {
+		t.Fatalf("got %v: expected %v", b, []byte("new_encoded_data"))
 	}
 }
 
 func TestExpiry(t *testing.T) {
 	m := New(time.Minute)
 
-	err := m.Save("test_session_token", []byte("encoded_data"), time.Now().Add(100*time.Millisecond))
+	err := m.Save("session_token", []byte("encoded_data"), time.Now().Add(100*time.Millisecond))
 	if err != nil {
 		t.Fatalf("got %v: expected %v", err, nil)
 	}
-	_, found, _ := m.Find("test_session_token")
+
+	_, found, _ := m.Find("session_token")
 	if found != true {
 		t.Fatalf("got %v: expected %v", found, true)
 	}
+
 	time.Sleep(100 * time.Millisecond)
-	_, found, _ = m.Find("test_session_token")
+	_, found, _ = m.Find("session_token")
 	if found != false {
 		t.Fatalf("got %v: expected %v", found, false)
 	}
 }
 
-func TestSave(t *testing.T) {
-	m := New(time.Minute)
-
-	err := m.Save("test_session_token", []byte("encoded_data"), time.Now().Add(time.Minute))
-	if err != nil {
-		t.Fatalf("got %v: expected %v", err, nil)
-	}
-	if len(m.Cache.Items()) != 1 {
-		t.Fatalf("got %d: expected %d", len(m.Cache.Items()), 1)
-	}
-}
-
 func TestDelete(t *testing.T) {
 	m := New(time.Minute)
-	m.Cache.Set("test_session_token", []byte("encoded_data"), 0)
+	m.Cache.Set("session_token", []byte("encoded_data"), 0)
 
-	err := m.Delete("test_session_token")
+	err := m.Delete("session_token")
 	if err != nil {
 		t.Fatalf("got %v: expected %v", err, nil)
 	}
-	if len(m.Cache.Items()) != 0 {
-		t.Fatalf("got %d: expected %d", len(m.Cache.Items()), 0)
+
+	_, found := m.Cache.Get("session_token")
+	if found != false {
+		t.Fatalf("got %v: expected %v", found, false)
 	}
 }

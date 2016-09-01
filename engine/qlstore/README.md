fb1faacbbafbfa377f05a3fd2aedbe0764dd4428
gernest <geofreyernest@live.com>
2016-09-01 13:35:37 +0300

    [feature] Add ql storage engine

---
+++ engine/qlstore/README.md
@@ -0,0 +1,128 @@
+
+# qlstore 
+[![godoc](https://godoc.org/github.com/alexedwards/scs/engine/qlstore?status.png)](https://godoc.org/github.com/alexedwards/scs/engine/qlstore)
+
+Package qlstore is a [ql](https://github.com/cznic/ql) storage engine for the [SCS session package](https://godoc.org/github.com/alexedwards/scs/session).
+
+## Usage
+
+### Installation
+
+Either:
+
+```
+$ go get github.com/alexedwards/scs/engine/qlstore
+```
+
+Or (recommended) use use [gvt](https://github.com/FiloSottile/gvt) to vendor the `engine/qlstore` and `session` sub-packages:
+
+```
+$ gvt fetch github.com/alexedwards/scs/engine/qlstore
+$ gvt fetch github.com/alexedwards/scs/session
+```
+
+### Setup
+
+You should have a working PostgreSQL database containing a `sessions` table with the definition:
+
+```sql
+	CREATE TABLE sessions (
+		token string,
+		data blob,
+		expiry  time
+	);
+CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+```
+
+### Example
+
+```go
+package main
+
+import (
+    "database/sql"
+    "io"
+    "log"
+    "net/http"
+    "time"
+
+    "github.com/alexedwards/scs/engine/qlstore"
+    "github.com/alexedwards/scs/session"
+)
+
+func main() {
+    // Establish a database/sql pool
+    db, err := sql.Open("ql", "test.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    // Create a new qlstore instance using the existing database/sql pool, 
+    // with a cleanup interval of 5 minutes.
+    engine := qlstore.New(db, 5*time.Minute)
+
+    sessionManager := session.Manage(engine)
+    http.HandleFunc("/put", putHandler)
+    http.HandleFunc("/get", getHandler)
+    http.ListenAndServe(":4000", sessionManager(http.DefaultServeMux))
+}
+
+func putHandler(w http.ResponseWriter, r *http.Request) {
+    err := session.PutString(r, "message", "Hello world!")
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+    }
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+    msg, err := session.GetString(r, "message")
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+    }
+    io.WriteString(w, msg)
+}
+```
+
+### Cleaning up expired session data
+
+The qlstore package provides a background 'cleanup' goroutine to delete expired session data. This stops the database table from holding on to invalid sessions indefinitely and growing unnecessarily large.
+
+You can specify how frequently to run the cleanup when creating a new qlstore instance:
+
+```go
+// Run a cleanup every 30 minutes.
+qlstore.New(db, 30*time.Minute)
+
+// Setting the cleanup interval to zero prevents the cleanup from being run.
+qlstore.New(db, 0)
+```
+
+#### Terminating the cleanup goroutine
+
+It's rare that the cleanup goroutine for a qlstore instance needs to be terminated. It is generally intended to be long-lived and run for the lifetime of your application.
+
+However, there may be occasions when your use of a qlstore instance is transient. A common example would be using it in a short-lived test function. In this scenario, the cleanup goroutine (which will run forever) will prevent the qlstore object from being garbage collected even after the test function has finished. You can prevent this by manually calling `StopCleanup()`.
+
+For example:
+
+```go
+func TestExample(t *testing.T) {
+    db, err := sql.Open("ql", "test.db")
+    if err != nil {
+        t.Fatal(err)
+    }
+    defer db.Close()
+
+    engine := New(db, time.Second)
+    defer engine.StopCleanup()
+
+    // Run test...
+}
+```
+
+## Notes
+
+The qlstore package is underpinned by the excellent [ql](https://github.com/cznic/ql) driver.
+
+Full godoc documentation: [https://godoc.org/github.com/alexedwards/scs/engine/qlstore](https://godoc.org/github.com/alexedwards/scs/engine/qlstore).
---
+++ engine/qlstore/ql_test.go
@@ -0,0 +1,338 @@
+package qlstore
+
+import (
+	"bytes"
+	"database/sql"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/session"
+)
+
+func TestNew(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+
+	p := New(db, 0)
+	_, ok := interface{}(p).(session.Engine)
+	if ok == false {
+		t.Fatalf("got %v: expected %v", ok, true)
+	}
+}
+func TestFind(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	migrate(t, db)
+	_, err = execTx(db, "TRUNCATE TABLE sessions")
+	if err != nil {
+		t.Fatal(err)
+	}
+	ex := time.Now().Add(time.Minute)
+	_, err = execTx(db,
+		`INSERT INTO sessions VALUES("session_token", $1,$2 )`,
+		[]byte("encoded_data"), ex)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := New(db, 0)
+
+	b, found, err := p.Find("session_token")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if found != true {
+		t.Fatalf("got %v: expected %v", found, true)
+	}
+	if bytes.Equal(b, []byte("encoded_data")) == false {
+		t.Fatalf("got %v: expected %v", b, []byte("encoded_data"))
+	}
+}
+
+func TestFindMissing(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	migrate(t, db)
+
+	p := New(db, 0)
+
+	_, found, err := p.Find("missing_session_token")
+	if err != nil {
+		t.Fatalf("got %v: expected %v", err, nil)
+	}
+	if found != false {
+		t.Fatalf("got %v: expected %v", found, false)
+	}
+}
+func TestSaveNew(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	migrate(t, db)
+
+	p := New(db, 0)
+
+	err = p.Save("session_token", []byte("encoded_data"), time.Now().Add(time.Minute))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	row := db.QueryRow(`SELECT data FROM sessions WHERE token = "session_token"`)
+	var data []byte
+	err = row.Scan(&data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if reflect.DeepEqual(data, []byte("encoded_data")) == false {
+		t.Fatalf("got %v: expected %v", data, []byte("encoded_data"))
+	}
+}
+
+func TestSaveUpdated(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	migrate(t, db)
+
+	ex := time.Now().Add(time.Minute)
+	_, err = execTx(db,
+		`INSERT INTO sessions VALUES("session_token", $1,$2 )`,
+		[]byte("encoded_data"), ex)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p := New(db, 0)
+
+	err = p.Save("session_token", []byte("new_encoded_data"), time.Now().Add(time.Minute))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	row := db.QueryRow(`SELECT data FROM sessions WHERE token = "session_token"`)
+	var data []byte
+	err = row.Scan(&data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if reflect.DeepEqual(data, []byte("new_encoded_data")) == false {
+		t.Fatalf("got %v: expected %v", data, []byte("new_encoded_data"))
+	}
+}
+
+func TestExpiry(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	migrate(t, db)
+
+	p := New(db, 0)
+
+	err = p.Save("session_token", []byte("encoded_data"), time.Now().Add(100*time.Millisecond))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, found, _ := p.Find("session_token")
+	if found != true {
+		t.Fatalf("got %v: expected %v", found, true)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+	_, found, _ = p.Find("session_token")
+	if found != false {
+		t.Fatalf("got %v: expected %v", found, false)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	migrate(t, db)
+
+	ex := time.Now().Add(time.Minute)
+	_, err = execTx(db,
+		`INSERT INTO sessions VALUES("session_token", $1,$2 )`,
+		[]byte("encoded_data"), ex)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := New(db, 0)
+
+	err = p.Delete("session_token")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	row := db.QueryRow(`SELECT count(*) FROM sessions WHERE token = "session_token"`)
+	var count int
+	err = row.Scan(&count)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatalf("got %d: expected %d", count, 0)
+	}
+}
+
+func TestCleanup(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	migrate(t, db)
+	_, err = execTx(db, "TRUNCATE TABLE sessions")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := New(db, 200*time.Millisecond)
+	defer p.StopCleanup()
+
+	err = p.Save("session_token", []byte("encoded_data"), time.Now().Add(100*time.Millisecond))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	row := db.QueryRow(`SELECT count(*) FROM sessions WHERE token = "session_token"`)
+	var count int
+	err = row.Scan(&count)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatalf("got %d: expected %d", count, 1)
+	}
+
+	time.Sleep(300 * time.Millisecond)
+	row = db.QueryRow(`SELECT count(*) FROM sessions WHERE token = "session_token"`)
+	err = row.Scan(&count)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatalf("got %d: expected %d", count, 0)
+	}
+}
+
+func TestStopNilCleanup(t *testing.T) {
+	dsn := os.Getenv("SESSION_QL_TEST_DSN")
+	if dsn == "" {
+		dsn = "test.db"
+	}
+	db, err := sql.Open("ql-mem", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err = db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	migrate(t, db)
+
+	p := New(db, 0)
+	time.Sleep(100 * time.Millisecond)
+	// A send to a nil channel will block forever
+	p.StopCleanup()
+}
+func migrate(t *testing.T, db *sql.DB) {
+	_, err := execTx(db, Table())
+	if err != nil {
+		t.Error(err)
+	}
+}

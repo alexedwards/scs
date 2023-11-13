d5d10bf94a0b65590b292703f367ab2836abf552
Clay Hayes <chayes3@emporia.edu>
2023-11-13 10:07:31 -0600

    Removed all println debugging

--- godrorstore/godrorstore.go
+++ godrorstore/godrorstore.go
@@ -25,7 +25,6 @@ func NewWithCleanupInterval(db *sql.DB, cleanupInterval time.Duration) *GodrorSt
 }
 
 func (g *GodrorStore) Find(token string) (b []byte, exists bool, err error) {
-	fmt.Println("Find")
 	stmt := fmt.Sprintf("SELECT data FROM sessions WHERE token = '%x' AND current_timestamp < expiry", token)
 	row := g.db.QueryRow(stmt)
 	err = row.Scan(&b)
@@ -38,12 +37,10 @@ func (g *GodrorStore) Find(token string) (b []byte, exists bool, err error) {
 }
 
 func (g *GodrorStore) Commit(token string, b []byte, expiry time.Time) error {
-	fmt.Println("Commit")
 	stmt := fmt.Sprintf("SELECT data FROM sessions WHERE token = '%x'", token)
 	row := g.db.QueryRow(stmt)
 	err := row.Err()
 	if row.Scan() == sql.ErrNoRows {
-		fmt.Println("No Row Found")
 		stmt = `INSERT INTO sessions (token, data, expiry) VALUES ('%x', '%x', to_timestamp('` + string(expiry.Format("2006-01-02 15:04:05.00")) + `', 'YYYY-MM-DD HH24:MI:SS.FF'))`
 		stmt = fmt.Sprintf(stmt, token, b)
 		fmt.Println(stmt)
@@ -53,11 +50,9 @@ func (g *GodrorStore) Commit(token string, b []byte, expiry time.Time) error {
 		}
 		return nil
 	} else if err != nil {
-		fmt.Println("Other error")
 		return err
 	}
 
-	fmt.Println("Update session data")
 	stmt = `UPDATE sessions SET data = '%x', expiry = to_timestamp('` + string(expiry.Format("2006-01-02 15:04:05.00")) + `', 'YYYY-MM-DD HH24:MI:SS.FF') WHERE token = '%x'`
 	stmt = fmt.Sprintf(stmt, b, token)
 	_, err = g.db.Exec(stmt)
@@ -69,14 +64,12 @@ func (g *GodrorStore) Commit(token string, b []byte, expiry time.Time) error {
 }
 
 func (g *GodrorStore) Delete(token string) error {
-	fmt.Println("Delete")
 	stmt := fmt.Sprintf("DELETE FROM session WHERE token = '%x'", token)
 	_, err := g.db.Exec(stmt)
 	return err
 }
 
 func (g *GodrorStore) All() (map[string][]byte, error) {
-	fmt.Println("All")
 	rows, err := g.db.Query("SELECT token, data FROM sessions WHERE current_timestamp < expiry")
 	if err != nil {
 		return nil, err
@@ -108,7 +101,6 @@ func (g *GodrorStore) All() (map[string][]byte, error) {
 }
 
 func (g *GodrorStore) StartCleanup(interval time.Duration) {
-	fmt.Println("StartCleanup")
 	g.stopCleanup = make(chan bool)
 	ticker := time.NewTicker(interval)
 	for {
@@ -126,14 +118,12 @@ func (g *GodrorStore) StartCleanup(interval time.Duration) {
 }
 
 func (g *GodrorStore) StopCleanup() {
-	fmt.Println("StopCleanup")
 	if g.stopCleanup != nil {
 		g.stopCleanup <- true
 	}
 }
 
 func (g *GodrorStore) deleteExpired() error {
-	fmt.Println("deleteExpired")
 	_, err := g.db.Exec("DELETE FROM sessions WHERE expiry < current_timestamp")
 	return err
 }
